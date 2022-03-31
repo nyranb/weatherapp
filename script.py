53fab6d9ce155b3120c74e6299dc507985683733
Nyran Bonilla <nyran.b@gmail.com>
2022-03-30 22:44:09 -0400

    changed styling, added tabs for weather results page

--- script.py
+++ script.py
@@ -12,23 +12,30 @@ def render_results():
     zip_code = request.form['zipCode']
 
     api_key = '3d284bf0531bdddc51793cd4ff778cef'
-    data = get_weather_results(zip_code, api_key)
-    temp = "{0:.2f}".format(data["main"]["temp"])
-    feels_like = "{0:.2f}".format(data["main"]["feels_like"])
-    weather = data["weather"][0]["main"]
-    city = data["name"]
-    country = data["sys"]["country"]
-    icon = "https://openweathermap.org/img/wn/" + data["weather"][0]["icon"] + "@2x.png"
+    current_data = get_current_weather(zip_code, api_key)
+    daily_data = get_daily_weather(zip_code, api_key)
+    temp = "{0:.2f}".format(current_data["main"]["temp"])
+    feels_like = "{0:.2f}".format(current_data["main"]["feels_like"])
+    weather = current_data["weather"][0]["main"]
+    city = current_data["name"]
+    country = current_data["sys"]["country"]
+    icon = "https://openweathermap.org/img/wn/" + current_data["weather"][0]["icon"] + "@2x.png"
 
     return render_template('results.html',
                            city=city, country=country, temp=temp,
                            feels_like=feels_like, weather=weather, icon=icon)
 
 
-def get_weather_results(zip_code, api_key):
+def get_current_weather(zip_code, api_key):
     api_url = "https://api.openweathermap.org/data/2.5/weather?zip={}&units=imperial&appid={}".format(zip_code, api_key)
     r = requests.get(api_url)
     return r.json()
+
+def get_daily_weather(zip_code, api_key):
+    api_url = "https://api.openweathermap.org/data/2.5/forecast/daily?zip={}&units=imperial&appid={}".format(zip_code, api_key)
+    r = requests.get(api_url)
+    return r.json()
+
 
 
 if __name__ == '__main__':
